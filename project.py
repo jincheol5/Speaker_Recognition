49508c2a7824570f7b79a44f626cab1756d3c67f
jincheol5 <oscar4293@gmail.com>
2021-11-27 01:40:37 +0900

    modified project.py

--- project.py
+++ project.py
@@ -2,54 +2,52 @@ import librosa
 import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from sklearn.datasets import load_iris
 from sklearn.cluster import KMeans
 from sklearn.mixture import GaussianMixture
 
+
+#np.transpose
 #########################################################
 #음성 파일 mfcc 추출하기
 
 #Train File Load(16000Hz/16bits/Mono)
 audio1,sr1=librosa.load('data/F1.wav',sr=16000)
-audio2,sr2=librosa.load('data/F2.wav',sr=16000)
-audio3,sr3=librosa.load('data/M1.wav',sr=16000)
-audio4,sr4=librosa.load('data/M2.wav',sr=16000)
 
-#각 화자 음성파일 MFCC 추출,2차원 배열로 저장된다 
-mfcc1=librosa.feature.mfcc(y=audio1,sr=sr1,n_mfcc=12,n_fft=500)
-mfcc2=librosa.feature.mfcc(y=audio2,sr=sr2,n_mfcc=12,n_fft=500)
-mfcc3=librosa.feature.mfcc(y=audio3,sr=sr3,n_mfcc=12,n_fft=500)
-mfcc4=librosa.feature.mfcc(y=audio4,sr=sr4,n_mfcc=12,n_fft=500)
+
+#각 화자 음성파일 MFCC 추출,2차원 배열로 저장된다  n_fft=frame 몇으로 나눌지 
+mfcc1=librosa.feature.mfcc(y=audio1,sr=sr1,n_mfcc=24,n_fft=500)
+
+audio1,sr1=librosa.load('test_data/1.wav',sr=16000)
+
+
+t_mfcc1=librosa.feature.mfcc(y=audio1,sr=sr1,n_mfcc=24,n_fft=500)
+
+#편리한 data handling을 위해 pandas data frame으로 변환 
+mfccDF=pd.DataFrame(data=mfcc1)
+
+#print(mfccDF)
+
 #n_mfcc=음성파일 몇으로 나눌지,n_fft=몇개로 프레임 나눌지
 #########################################################
 
-gmm1 = GaussianMixture(n_components=5).fit(mfcc1)
-gmm1 = GaussianMixture(n_components=5).fit(mfcc2)
-gmm1 = GaussianMixture(n_components=5).fit(mfcc3)
-gmm1 = GaussianMixture(n_components=5).fit(mfcc4)
+gmm = GaussianMixture(n_components=5)
+
+gmm.fit(mfccDF)
 
 
+#gmm.predict(mfccDF)
 
-audio1,sr1=librosa.load('test_data/1.wav',sr=16000)
-audio2,sr2=librosa.load('test_data/2.wav',sr=16000)
-audio3,sr3=librosa.load('test_data/3.wav',sr=16000)
-audio4,sr4=librosa.load('test_data/4.wav',sr=16000)
+#gmm.score(t_mfcc1) #score 안에는 testdata의 mfcc 값을 넣는다  결과값은 log를 한  확률값이 나온다 
 
-t_mfcc1=librosa.feature.mfcc(y=audio1,sr=sr1,n_mfcc=12,n_fft=500)
-t_mfcc2=librosa.feature.mfcc(y=audio2,sr=sr2,n_mfcc=12,n_fft=500)
-t_mfcc3=librosa.feature.mfcc(y=audio3,sr=sr3,n_mfcc=12,n_fft=500)
-t_mfcc4=librosa.feature.mfcc(y=audio4,sr=sr4,n_mfcc=12,n_fft=500)
+
+print(gmm.score(t_mfcc1))
+
+
 
 
 #n_components = mixture 개수 
 #gmm_cluster_labels = gmm.predict(mfcc1) #트레이닝
 #입력 데이터 = mfcc
-
-print()
-
-
-print(gmm1.score(t_mfcc1))
-print(gmm1.score(t_mfcc2))
-print(gmm1.score(t_mfcc3))
-print(gmm1.score(t_mfcc4))
